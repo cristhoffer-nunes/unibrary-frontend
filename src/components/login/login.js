0cc2c9eabad8e4d697e86df926d992d0d40a5707
Cristhoffer Nunes <nunes-santos@hotmail.com>
2021-10-13 13:45:56 -0300

    Sistema de login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,38 +1,61 @@
-import React from 'react';
+import React from 'react'
+
+import { ErrorMessage, Formik, Form, Field } from 'formik'
+import axios from 'axios'
+import { history } from '../../history'
+
 import { Container } from 'react-bootstrap';
 import '../../App.css';
 import imgLogin from '../../img/imgLogin.png'
 
 
 
-function login (){
+const Login = () => {
+    const handleSubmit = (values) => {
+        axios.post("http://localhost:8080/v1/api/auth", {
+            numeroMatricula: values.numeroMatricula,
+            senha: values.senha,
+        }).then((response) => {
+            if (response.status === 200) {
+                const { data } = response
+                if (data) {
+                    localStorage.setItem('app-token', data.result)
+                    history.push('/home')
+                    window.location.reload()
+                }
 
-        return(
+
+            }
+            else history.push('/login')
+        })
+    };
+
+
+    return (
         <Container className="corpo">
-
             <div className="container-login">
-
             <img src={imgLogin} className="foto-login"></img>
+            <Formik initialValues={{}} onSubmit={handleSubmit}>
 
-            <div className="form">
-                <div className="titulo-form">
-                    LOGIN
-                </div>
-                <form>
-                <label>
-                    <div className="input-txt">Número de Matrícula:</div>
-                    <input type="text" name="num" className="input-dado" required/>
-                </label>
-                <label>
-                    <div className="input-txt">Senha:</div>
-                    <input type="password" name="senha" className="input-dado" required/>
-                </label>
-                <button type="submit" className="botn btn2">Entrar</button>
-                </form>
+                <Form className="Login">
+                    <div className="titulo-form">
+                        LOGIN
+                    </div>
+                    <label>
+                        <div className="input-txt">Número de Matrícula:</div>
+                        <Field name="numeroMatricula" className="input-dado" />
+                    </label>
+                    <label>
+                        <div className="input-txt">Senha:</div>
+                        <Field name="senha" className="input-dado" />
+                    </label>
+
+                    <button type="submit" className="botn btn2">Entrar</button>
+                </Form>
+            </Formik>
             </div>
-            </div>
-        </Container>  
-        )  
+        </Container>
+    )
 }
 
-export default login;
+export default Login
